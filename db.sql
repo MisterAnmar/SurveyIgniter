be210c119c46b25162d074427876af7abe7347c1
AnmarDev <59205168+anmar-dev@users.noreply.github.com>
2020-03-29 06:28:52 +0300

    Refactor DB schema to accomedate Constrains and relashonship

--- db.sql
+++ db.sql
@@ -1,146 +1,71 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.26 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5919
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Dumping database structure for surveyignitor1
+CREATE DATABASE IF NOT EXISTS `surveyignitor1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `surveyignitor1`;
 
+SET FOREIGN_KEY_CHECKS = 0;
 
--- Dumping database structure for surveyigniter
-CREATE DATABASE IF NOT EXISTS `surveyigniter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `surveyigniter`;
-
--- Dumping structure for table surveyigniter.invitation
-CREATE TABLE IF NOT EXISTS `invitation` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
+-- Dumping structure for table surveyignitor1.option
+DROP TABLE IF EXISTS `option`;
+CREATE TABLE IF NOT EXISTS `option` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `question_id` bigint(20) unsigned NOT NULL,
+  `option` varchar(200) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='Proposed to handle invitations for users to answer the survey';
+  PRIMARY KEY (`id`),
+  KEY `FK1_question` (`question_id`),
+  CONSTRAINT `FK1_question` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table surveyigniter.option
-CREATE TABLE IF NOT EXISTS `option` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL,
-  `survey_id` bigint(20) unsigned NOT NULL,
-  `question_id` bigint(20) unsigned NOT NULL,
-  `option` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='handles possible options of questions for radio type and select (multiselct questions)';
-
--- Data exporting was unselected.
-
--- Dumping structure for table surveyigniter.question
+-- Dumping structure for table surveyignitor1.question
+DROP TABLE IF EXISTS `question`;
 CREATE TABLE IF NOT EXISTS `question` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL,
-  `survey_id` bigint(20) unsigned NOT NULL,
-  `question` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` enum('textarea','radio','checkbox') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'textarea',
+  `user_id` int(10) unsigned NOT NULL,
+  `survey_id` int(10) unsigned NOT NULL,
+  `content` varchar(250) NOT NULL,
+  `type` enum('textarea','radio','checkbox') NOT NULL DEFAULT 'textarea',
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='table to store survey questions and its type (textarea. radio, select).';
+  PRIMARY KEY (`id`),
+  KEY `FK1_survey` (`survey_id`),
+  CONSTRAINT `FK1_survey` FOREIGN KEY (`survey_id`) REFERENCES `survey` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table surveyigniter.respondent
-CREATE TABLE IF NOT EXISTS `respondent` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='stores each respondant to survey (needs enhancement) a way to identify a unique respondent. so far based on email';
-
--- Data exporting was unselected.
-
--- Dumping structure for table surveyigniter.resp_radio
-CREATE TABLE IF NOT EXISTS `resp_radio` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `survey_id` bigint(20) unsigned NOT NULL,
-  `question_id` bigint(20) unsigned NOT NULL,
-  `option_id` bigint(20) unsigned NOT NULL,
-  `respondent_id` bigint(20) unsigned NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='stores respondent of radio type questions';
-
--- Data exporting was unselected.
-
--- Dumping structure for table surveyigniter.resp_select
-CREATE TABLE IF NOT EXISTS `resp_select` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `survey_id` bigint(20) unsigned NOT NULL,
-  `question_id` bigint(20) unsigned NOT NULL,
-  `option_id` bigint(20) unsigned NOT NULL,
-  `respondent_id` bigint(20) unsigned NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='stores respondent of select (multiselect) type questions';
-
--- Data exporting was unselected.
-
--- Dumping structure for table surveyigniter.resp_textarea
-CREATE TABLE IF NOT EXISTS `resp_textarea` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `survey_id` bigint(20) unsigned NOT NULL,
-  `question_id` bigint(20) unsigned NOT NULL,
-  `user_id` bigint(20) unsigned NOT NULL,
-  `respondent_id` bigint(20) unsigned NOT NULL,
-  `answer` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='stores respondent of textarea type questions';
-
--- Data exporting was unselected.
-
--- Dumping structure for table surveyigniter.survey
+-- Dumping structure for table surveyignitor1.survey
+DROP TABLE IF EXISTS `survey`;
 CREATE TABLE IF NOT EXISTS `survey` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL,
-  `title` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK1_user` (`user_id`),
+  CONSTRAINT `FK1_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table surveyignitor1.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='stores surveys and who owns it.';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
-
--- Dumping structure for table surveyigniter.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(200) CHARACTER SET latin1 NOT NULL,
-  `name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(200) CHARACTER SET latin1 NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='draft table for users (account)';
-
--- Data exporting was unselected.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+SET FOREIGN_KEY_CHECKS = 1;
